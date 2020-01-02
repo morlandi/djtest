c51156e12fe808554258d6351cb70325a94f25ef
Mario Orlandi <morlandi@brainstorm.it>
2020-01-02 12:07:53 +0100

    Create sample test settings files

--- djtest/djtest.py
+++ djtest/djtest.py
@@ -100,10 +100,67 @@ apps=app1, app2
         if query_yes_no('Create default config file "%s" ?' % config_filename):
             create_default_config_file(config_filename)
             print('Default config file "%s" has been created; please check it before running this script again' % config_filename)
+            create_sample_test_settings()
+            print('Default sample "test_setting.py" and "test_settings_no_migrations.py" have been created;')
+            print('adjust them according to your needs and move to a suitable folder')
         exit(-1)
 
     return config
 
+
+def create_sample_test_settings():
+
+    test_settings = """
+from .settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+COMPRESS_ENABLED = False
+COMPRESS_OFFLINE = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "77777777777777777777777777777777777777777777777777"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
+        'NAME': ':memory:',
+    }
+}
+
+#INSTALLED_APPS.remove('easyaudit')
+
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'public', 'test_media'))
+
+THUMBNAIL_DEBUG = True
+
+RQ_PREFIX = "test"
+RQ_QUEUES = setup_rq_queues(RQ_PREFIX)
+
+RQ_SHOW_ADMIN_LINK = True
+DJANGOTASK_LOG_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'protected', 'test_tasklog'))
+DJANGOTASK_ALWAYS_EAGER = True
+DJANGOTASK_JOB_TRACE_ENABLED = True
+"""
+
+    test_settings_no_migrations = """
+from .test_settings import *
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+    def __getitem__(self, item):
+        return None
+        #return "notmigrations"
+MIGRATION_MODULES = DisableMigrations()
+"""
+
+    with open("test_settings.py", 'w') as f:
+        f.write(test_settings)
+    with open("test_settings_no_migrations.py", 'w') as f:
+        f.write(test_settings_no_migrations)
 
 def main():
 
